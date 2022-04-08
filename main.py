84c8f3dd50dae7294635b4d1868d3b71254c2311
ksujecki <kpsujecki@gmail.com>
2022-04-08 13:07:47 +0200

    added logging, some refactor code

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import argparse
 import logging
 from itertools import islice
 from scripts.sql import trackTable, listeningTable, getTracks, getArtists
+from datetime import datetime
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 parser = argparse.ArgumentParser()
 
@@ -13,21 +19,26 @@ args = parser.parse_args()
 
 
 def readTrackFromFiles():
-    with open(args.uniqueTracks, 'rb') as f:
+    with open(args.uniqueTracks, 'r', newline='\n', encoding='ISO-8859-1') as f:
         lines = f.readlines()
+        lines = [line.replace('\n', '') for line in lines]
         for line in lines:
-            fields = line.split("<SEP>".encode())
+            fields = line.split("<SEP>")
             cur.execute("""INSERT INTO track (id, id_track, artistName, trackName) VALUES (?, ?, ?, ?)""",
                         (fields[0], fields[1], str(fields[2]), str(fields[3])))
+            logging.debug("ID: {} IDTrack: {} ARTISTNAME: {} TRACKNAME: {}".format(fields[0], fields[1], fields[2], fields[3]))
         logging.info('Songs read from file')
 
 
 def readListeningsFromFiles():
-    with open(args.tripletsSample, 'rb') as f:
+    with open(args.tripletsSample, 'r', newline='\n', encoding='ISO-8859-1') as f:
         lines = f.readlines()
+        lines = [line.replace('\n', '') for line in lines]
         for line in lines:
-            fields = line.split("<SEP>".encode())
-            cur.execute('INSERT INTO listening VALUES(?, ?, ?, ?);', (None, fields[0], fields[1], fields[2]))
+            fields = line.split("<SEP>")
+            dateListen = datetime.fromtimestamp((int(fields[2])))
+            cur.execute('INSERT INTO listening VALUES(?, ?, ?, ?);', (None, fields[0], fields[1], dateListen))
+            logging.debug("IDUser {} IDTrack {} DATE {}".format(fields[0], fields[1], dateListen))
         logging.info('Listenings read from file')
 
 
@@ -49,14 +60,16 @@ def getDataFromDatabase():
 
 def showTheMostPopularTracks(mostpopulartracks):
     mostpopulartracks = islice(mostpopulartracks, 5)
-    for mostpopulartrack in mostpopulartracks:
-        logging.info("Track: " + mostpopulartrack[1] + " The number of listenings: ", mostpopulartrack[0])
+    print("The most popular tracks:")
+    for idx, mostpopulartrack in enumerate(mostpopulartracks):
+        print("{}. Track: {} The number of listenings {}".format(idx+1, mostpopulartrack[1], mostpopulartrack[0]))
 
 
 def showTheMostPopularArtist(mostpopulartartists):
     mostpopulartartists = islice(mostpopulartartists, 5)
-    for mostpopulartartist in mostpopulartartists:
-        logging.info("Artist: " + mostpopulartartist[1] + " The number of listenings: ", mostpopulartartist[0])
+    print("The most popular Artists/Bands:")
+    for idx, mostpopulartartist in enumerate(mostpopulartartists):
+        print("{}. Artist: {} The number of listenings {}".format(idx+1, mostpopulartartist[1], mostpopulartartist[0]))
 
 
 if __name__ == '__main__':
@@ -66,6 +79,7 @@ if __name__ == '__main__':
     createTablesInDatabase()
     readTrackFromFiles()
     readListeningsFromFiles()
-    getDataFromDatabase()
     con.commit()
+    getDataFromDatabase()
+    logging.info("Saved in database")
     logging.info("Done")
